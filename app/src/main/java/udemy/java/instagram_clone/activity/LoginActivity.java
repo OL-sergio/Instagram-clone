83dd39a29951699250ef8f6f398f803b03746d9c
Ol-sergio <a18074@alunos.ipca.pt>
2023-06-26 16:38:59 +0100

    Work Done: Adding data binding model and adding intent to change activity.
    * Notes:
    - ...
    
    * Add items:
    - User
    
    * Updated items:
    - LoginActivity
    - build.gradle
    - AndroidManifest.xml
    
    * Removed:
    ...

--- app/src/main/java/udemy/java/instagram_clone/activity/LoginActivity.java
+++ app/src/main/java/udemy/java/instagram_clone/activity/LoginActivity.java
@@ -1,16 +1,41 @@
 package udemy.java.instagram_clone.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import udemy.java.instagram_clone.R;
+import udemy.java.instagram_clone.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    private TextView goToCreateUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+       goToCreateUser = binding.textViewCreateAccount;
+
+        goToCreateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity( new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
     }
 }
