2b441cca4b68eb457b694364911fcfe6ae8097d7
Ol-sergio <a18074@alunos.ipca.pt>
2023-06-30 18:30:10 +0100

    Work Done: Creating a custom toolbar and adding the code that gives to user option to log out from app.
    * Notes:
    - ...
    
    * Add items:
    - menu_main.xml
    - toolbar_main.xml
    
    * Updated items:
    - MainActivity
    - AndroidManifest
    - activity_main
    - strings
    - themes
    - colors
    
    * Removed:
    ...

--- app/src/main/java/udemy/java/instagram_clone/activity/MainActivity.java
+++ app/src/main/java/udemy/java/instagram_clone/activity/MainActivity.java
@@ -1,16 +1,76 @@
 package udemy.java.instagram_clone.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import udemy.java.instagram_clone.R;
+import udemy.java.instagram_clone.config.FirebaseConfiguration;
+import udemy.java.instagram_clone.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private FirebaseAuth firebaseAuth;
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        toolbar = binding.toolbarMain.toolbarMain;
+        toolbar.setTitle("Instagram");
+        setSupportActionBar(toolbar);
+
+        firebaseAuth = FirebaseConfiguration.getUserAuthentication();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.menu_logOut) {
+            logOutUser();
+            startActivity( new Intent(MainActivity.this, LoginActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logOutUser() {
+        try {
+
+            firebaseAuth.signOut();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
