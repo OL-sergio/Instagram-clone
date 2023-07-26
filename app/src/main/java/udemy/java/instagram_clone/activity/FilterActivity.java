6bad1681f33e6ae858356b139b7e03b4d30cd7e4
Ol-sergio <a18074@alunos.ipca.pt>
2023-07-26 15:36:38 +0100

    Work Done: Adding the code that verifies if you follow or followers to another user.
    * Notes:
    - ...
    
    * Add items:
    - ...
    
    * Updated items:
    - FriendsProfileActivity
    - build.gradle
    
    * Removed:
    - ...

--- app/src/main/java/udemy/java/instagram_clone/activity/FilterActivity.java
+++ app/src/main/java/udemy/java/instagram_clone/activity/FilterActivity.java
@@ -14,6 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
+import com.zomato.photofilters.SampleFilters;
+import com.zomato.photofilters.imageprocessors.Filter;
 
 import java.util.Objects;
 
@@ -22,10 +24,16 @@ import udemy.java.instagram_clone.databinding.ActivityFilterBinding;
 
 public class FilterActivity extends AppCompatActivity {
 
+    static
+    {
+        System.loadLibrary("NativeImageProcessor");
+    }
+
     private ActivityFilterBinding binding;
 
     private ImageView imageViewSelectedPhoto;
     private Bitmap image = null;
+    private Bitmap imageFilter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +59,42 @@ public class FilterActivity extends AppCompatActivity {
             byte [] dataImage = bundle.getByteArray( "selectedPhoto" );
             image = BitmapFactory.decodeByteArray (dataImage, 0 , dataImage.length );
 
-            Glide.with(FilterActivity.this)
+            /*Glide.with(FilterActivity.this)
                     .load(image)
                     .placeholder(R.drawable.avatar)
+                    .into(imageViewSelectedPhoto);*/
+
+            imageFilter = image.copy( image.getConfig(),true );
+            Filter myFilter = new Filter();
+
+
+            //myFilter.addSubFilter(new BrightnessSubFilter(50));
+            //myFilter.addSubFilter(new ContrastSubFilter(2.5f));
+            //myFilter.addSubFilter(new SaturationSubFilter(9));
+            //myFilter.addSubFilter( new ColorOverlaySubFilter(12,5f,2f,2f));
+
+            /*
+            *Point[] rgbKnots;
+            rgbKnots = new Point[3];
+            rgbKnots[0] = new Point(0, 0);
+            rgbKnots[1] = new Point(15, 100);
+            rgbKnots[2] = new Point(25, 125);
+            myFilter.addSubFilter( new ToneCurveSubFilter(null,null,null,null));
+            */
+
+            //myFilter.addSubFilter( new BrightnessSubFilter(150));
+            //myFilter.addSubFilter( new VignetteSubFilter(getBaseContext(), 50 ));
+
+            Filter fooFilter = SampleFilters.getStarLitFilter();
+            Bitmap outputImage = fooFilter.processFilter(imageFilter);
+
+            Glide.with(FilterActivity.this)
+                    .load(outputImage)
+                    .placeholder(R.drawable.avatar)
                     .into(imageViewSelectedPhoto);
+
+
+
 
         }
     }
